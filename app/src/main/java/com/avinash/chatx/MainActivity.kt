10957450fe3b3d98c1c41017f27d30675d795e07
avinashbest <avinashbest3@gmail.com>
2021-09-07 21:35:45 +0530

    Enabled: buildFeatures viewBinding

--- app/src/main/java/com/avinash/chatx/MainActivity.kt
+++ app/src/main/java/com/avinash/chatx/MainActivity.kt
@@ -2,10 +2,12 @@ package com.avinash.chatx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avinash.chatx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
