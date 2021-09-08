342eed7482b8e0e14c7ec5d3bb295fceb5aab6b5
avinashbest <avinashbest3@gmail.com>
2021-09-08 23:50:06 +0530

    Created: Login Fragment Interface

---
+++ app/src/main/java/com/avinash/chatx/AuthenticationActivity.kt
@@ -0,0 +1,21 @@
+package com.avinash.chatx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avinash.chatx.databinding.ActivityAuthenticationBinding
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*
+        * As soon as user started, taking him/her to the login fragment
+        * */
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.auth_fragmentContainer, LoginFragment())
+            .commit()
+    }
+}
--- app/src/main/java/com/avinash/chatx/MainActivity.kt
+++ app/src/main/java/com/avinash/chatx/MainActivity.kt
@@ -2,12 +2,41 @@ package com.avinash.chatx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.avinash.chatx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnIncrease.setOnClickListener {
+            var value = Integer.parseInt(binding.textView.text.toString())
+            value++
+            binding.textView.text = value.toString()
+        }
+
+        binding.btnDecrease.setOnClickListener {
+            var value = Integer.parseInt(binding.textView.text.toString())
+            value--
+            binding.textView.text = value.toString()
+        }
+    }
+
+    /*
+    * For saving the state in android whenever, landscape to portrait or any activity change happens
+    * */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Value", binding.textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.textView.text = savedInstanceState.getString("Value")
     }
 }
